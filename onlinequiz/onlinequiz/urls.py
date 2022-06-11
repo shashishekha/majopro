6fe65aec00991636948176f74ccdc3afa072460e
shashi shekhar <shashiyadav7629518@gmail.com>
2022-06-11 08:52:42 +0530

    student

--- onlinequiz/onlinequiz/urls.py
+++ onlinequiz/onlinequiz/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('student/',include('student.urls')),
+    path('student/',include('student.urls')),
 
     # path('',views.home_view,name=''),
 
